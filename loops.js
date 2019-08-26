d66f79a222450aeb6d57bcf3b1303779b9d7661f
Donal tun <americanarakan@gmail.com>
2019-08-26 01:27:13 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,10 +1,9 @@
 
 function forLoop(array){
   for( var i = 0; i =< 25 ; i++){
-    if (i ===1 ){
+    if (i === 1 ){
       array.push(`I am 1 strange loop`)
-    }else
-    {`I am ${i} strange loop.`}
+    }else {array.push(`I am ${i} strange loop.`)}
 
       }
     }
