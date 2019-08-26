92a10df96f3e153b2555f08b9276607a44e3d1e7
Donal tun <americanarakan@gmail.com>
2019-08-26 02:26:52 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -17,14 +17,3 @@ function whileLoop(n){
   }
   return "done";
 }
-
-function doWhileLoop(array) {
-  function maybeTrue() {
-    return Math.random() >= 0.5
-  }
-
-  do {
-    array.pop();
-  } while (array.length > -1 && maybeTrue());
-    return array;
-}
